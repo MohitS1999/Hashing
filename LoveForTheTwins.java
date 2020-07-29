208fbf00939cfb968a9da4f8d441d5e35306d3bd
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-07-29 22:19:45 +0530

    Basic

---
+++ LoveForTheTwins.java
@@ -0,0 +1,33 @@
+/*package whatever //do not write package name here */
+//https://practice.geeksforgeeks.org/problems/love-for-the-twins/0
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class LoveForTheTwins{
+	public static void main (String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine().trim());
+		while (t-->0){
+		    int n=Integer.parseInt(br.readLine().trim());
+		    int ar[]=new int[n];
+		    String[] str=br.readLine().split(" ");
+		    for (int i=0;i<n;i++){
+		        ar[i]=Integer.parseInt(str[i]);
+		    }
+		    HashMap<Integer,Integer> hm=new HashMap<>();
+		    for (int i=0;i<n;i++){
+		        int a=ar[i];
+		        if (hm.containsKey(a))
+		            hm.put(a,hm.get(a)+1);
+		        else
+		            hm.put(a,1);
+		    }
+		    int cnt=0;
+		    for (Map.Entry<Integer,Integer> entry:hm.entrySet()){
+		        cnt+=entry.getValue()/2;
+		    }
+		    System.out.println(cnt*2);
+		}
+	}
+}
